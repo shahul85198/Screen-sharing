e0dcd65c77a87acc4e50a98258cc22724d7b67cd
Shami <skmasthan.1999@gmail.com>
2024-02-29 20:07:17 +0530

    Page-edit

---
+++ screen-share/src/Edit/develope.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Develop(){
+        return(
+                
+                <div class='id' > 
+
+                        
+                       <div class ='key'>
+
+                       <h1 class ='content'> 
+                                Screen recording
+                         </h1>
+                    <img src= 'https://cdn.iconscout.com/icon/premium/png-256-thumb/screen-sharing-4-789936.png' alt='' />
+                        
+                         <h3> Create animated GIFs from a screen recording. </h3>
+
+                         <button class='button'>
+                                Click to Recording 
+                         </button>                         
+
+                       </div>
+                       
+                       
+
+                </div>
+                  
+        )
+}
