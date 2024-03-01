7df43568a70508bfb65b3f29b0ad8e50d9b65a5c
Shami <skmasthan.1999@gmail.com>
2024-03-01 14:41:36 +0530

    add

--- screen-share/src/Edit/develope.js
+++ screen-share/src/Edit/develope.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Develop(){
         return(
                 
-                <div class='id' > 
+        <div class='id' > 
 
                         
                        <div class ='key'>
